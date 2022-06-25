ed1b36e6283584db13dd229e0aa737071b83cf09
Awaab Elamin <AwaabElamin@gmail.com>
2022-06-25 16:02:42 -0500

    update app.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { Login } from './components/Login';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+      <Login/>
+      </header>
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -17,9 +20,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
     </div>
   );
 }
-
 export default App;
