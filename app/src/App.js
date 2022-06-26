15f912f1845fd41967c65bd32f9f54862f54a9dc
Awaab Elamin <AwaabElamin@gmail.com>
2022-06-25 22:58:18 -0500

    add router with provider

--- app/src/App.js
+++ app/src/App.js
@@ -1,28 +1,48 @@
 import logo from './logo.svg';
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
-import { Login } from './components/Login';
+import { Login, Register } from './components/Login';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { useState } from 'react';
+import { configureStore } from "@reduxjs/toolkit";
 
 function App() {
+  const value = configureStore({
+    reducer: {
+      test: 'test'
+    }
+
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <div className="Position">
-        <Login />
-        </div>
-      
-      </header>
+
+      <BrowserRouter>
+        <Provider store={value}>
+          <header className="App-header">
+            <div className="LoginPosition">
+              <Login />
+            </div>
+          </header>
+
+          <Routes>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/register" element={<Register />}></Route>
+          </Routes>
+        </Provider>
+      </BrowserRouter>
       <img src={logo} className="App-logo" alt="logo" />
       <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
     </div>
   );
 }
