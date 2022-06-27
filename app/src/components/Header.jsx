a4abf837ba5a994e86a2630dfe0c8dd76024e622
Awaab Elamin <AwaabElamin@gmail.com>
2022-06-26 21:47:51 -0500

    collect all header statments in one component

---
+++ app/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import { Login } from './Login';
+export function Header(){
+    return(
+        <header className="App-header">
+        <div className="LoginPosition">
+          <Login />
+        </div>
+      </header>
+    )
+}
