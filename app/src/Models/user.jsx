6e795409389093f7fafa8c8a5c02c029d3089548
Awaab Elamin <AwaabElamin@gmail.com>
2022-06-26 21:50:28 -0500

    user now conatins login, auth and register components

--- app/src/Models/user.jsx
+++ app/src/Models/user.jsx
@@ -1,7 +1,12 @@
-import react from "react";
+// import react from "react";
 import axios from 'axios';
-const URL = "localhost:3001";
-export async function LoginAuth (username, password){
-    const data = await axios.post(URL+ '/login',{username:'admin', password:'admin'});
-    console.log(data);
+const URL = "http://localhost:3001";
+export async function LoginAuth(username, password) {
+    let result = '';
+    await axios.post(URL + '/login', { username: username, password: password })
+        .then(res => {
+            result = res.data;
+        })
+        .catch(err => result = 'error:-\n' + err);
+    return result;
 }
