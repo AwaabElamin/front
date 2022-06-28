3ee0d7c72100ba54e766f46a7348c2cf7122d15b
Awaab Elamin <AwaabElamin@gmail.com>
2022-06-27 21:01:38 -0500

    add book link to Header

--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -1,10 +1,15 @@
 import { Login } from './Login';
-export function Header(){
-    return(
-        <header className="App-header">
-        <div className="LoginPosition">
-          <Login />
-        </div>
-      </header>
-    )
+import { Link } from "react-router-dom";
+import '../App.css';
+export function Header() {
+  return (
+    <header className="App-header">
+      <div className="LoginPosition">
+        <Login />
+      </div>
+      <nav className='navLeft'>
+        <Link className='App-link' to={`/book/add`}>add a book</Link>
+      </nav>
+    </header>
+  )
 }
