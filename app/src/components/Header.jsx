0f88eb3622861fae1caf04bd245d7a48cf1cf7df
Awaab Elamin <AwaabElamin@gmail.com>
2022-06-30 16:38:29 -0500

    shows link depend on role

--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -1,7 +1,12 @@
+import { useEffect, useState } from "react";
 import { Login } from './users';
 import { Link } from "react-router-dom";
 import '../App.css';
 export function Header() {
+  const [role, setRole] = useState('');
+  useEffect(() => {
+    setRole(localStorage.getItem('role'));
+  }, []);
   return (
     <header className="App-header">
       <div className="LoginPosition">
@@ -11,20 +16,27 @@ export function Header() {
         <ul>
           <li><Link className='App-link' to={`/book`}>books</Link></li>
           <li><Link className='App-link' to={`/book/find`}>find a book</Link></li>
-          <li><Link className='App-link' to={`/book/add`}>add a book</Link></li>
-          <li><Link className='App-link' to={`/book/update`}>update book</Link></li>
+          {role == 'admin' ?
+            <>
+              <li><Link className='App-link' to={`/book/add`}>add a book</Link></li>
+              <li><Link className='App-link' to={`/book/update`}>update book</Link></li>
+            </>
+
+            : null}
+
         </ul>
       </nav>
       <nav className='navLeft'>
         <ul>
-          <li><Link className='App-link' to={`/users`}>users</Link></li>
+          {role === 'admin' ?
+            <li><Link className='App-link' to={`/users`}>users</Link></li>
+            : null}
           <li><Link className='App-link' to={`/register`}>register</Link></li>
         </ul>
       </nav>
       <nav className='navLeft'>
         <ul>
           <li><Link className='App-link' to={`/borrow`}>borrows</Link></li>
-          <li><Link className='App-link' to={`/borrow/add`}>borrow_add</Link></li>
         </ul>
       </nav>
       <nav className='navLeft'>
