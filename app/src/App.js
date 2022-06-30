fd71bdb0bea4f69c0160d3b147ae631b99c24d0b
Awaab Elamin <AwaabElamin@gmail.com>
2022-06-30 13:21:08 -0500

    update app main page

--- app/src/App.js
+++ app/src/App.js
@@ -13,8 +13,7 @@ function App() {
     <div className="App">
 
       <BrowserRouter>
-         <Header/>
-          
+         <Header/>          
           <Routes>
             <Route exact path="/" element={''}></Route>
             <Route exact path="/login" element={<Login />}></Route>
@@ -28,19 +27,7 @@ function App() {
             <Route path="/borrow/add" element={<BorrowBook />}></Route>
             <Route path="/card" element={<Card />}></Route>
           </Routes>
-      </BrowserRouter>
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
+      </BrowserRouter>     
     </div>
   );
 }
